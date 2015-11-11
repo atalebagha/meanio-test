cd56f410ffbb3f5c9cac96e1c0adbccc2e0d001c
Abdul-Rahman Taleb-Agha <abdulrahman@talebagha.net>
2015-11-11 15:51:19 -0800

    created project model

---
+++ packages/custom/projects/server/models/project.js
@@ -0,0 +1,75 @@
+'use strict';
+
+/**
+ * Module dependencies.
+ */
+var mongoose = require('mongoose'),
+  Schema = mongoose.Schema;
+
+
+/**
+ * Project Schema
+ */
+var ProjectSchema = new Schema({
+  created: {
+    type: Date,
+    default: Date.now
+  },
+  due: {
+    type: Date,
+    required: true
+  },
+  title: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  description: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  user: {
+    type: Schema.ObjectId,
+    ref: 'User'
+  },
+  permissions: {
+    type: Array
+  },
+  updated: {
+    type: Array
+  }
+});
+
+/**
+ * Validations
+ */
+ProjectSchema.path('title').validate(function(title) {
+  return !!title;
+}, 'Title cannot be blank');
+
+ProjectSchema.path('description').validate(function(description) {
+  return !!description;
+}, 'Title cannot be blank');
+
+ProjectSchema.path('due').validate(function(due) {
+  return !!due;
+}, 'Title cannot be blank');
+
+ProjectSchema.path('due').validate(function(due) {
+
+  return due-Date.now > 0;
+}, 'Date must be a future date');
+
+
+
+/**
+ * Statics
+ */
+ProjectSchema.statics.load = function(id, cb) {
+  this.findOne({
+    _id: id
+  }).populate('user', 'name username').exec(cb);
+};
+
+mongoose.model('Project', ProjectSchema);
